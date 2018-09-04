f1e9a24aa0bb6af8a9cc217f4f82d4bf05f5c07c
Lieuwe Helmus <lhelmus@hashicorp.com>
2018-09-04 11:39:17 +0200

    Delete resource

--- main.tf
+++ main.tf
@@ -2,13 +2,13 @@ provider "aws" {
   region     = "us-east-1"
 }
 
-resource "aws_instance" "example" {
-  ami           = "ami-2757f631"
-  instance_type = "t2.micro"
-
-  tags {
-    Name = "Lieuwe's Testing"
-    owner = "lhelmus@hashicorp.com"
-    TTL = 48
-  }
-}
+# resource "aws_instance" "example" {
+#   ami           = "ami-2757f631"
+#   instance_type = "t2.micro"
+#
+#   tags {
+#     Name = "Lieuwe's Testing"
+#     owner = "lhelmus@hashicorp.com"
+#     TTL = 48
+#   }
+# }
