4734b8200c35041312a49dc9e9115adfecdcd4bd
Lieuwe Helmus <lhelmus@hashicorp.com>
2018-09-05 10:56:37 +0200

    Deplyment Test

--- main.tf
+++ main.tf
@@ -1,5 +1,11 @@
 provider "aws" {
-  region     = "us-east-1"
+  region = "us-east-1"
+  alias  = "us-east-1"
+}
+
+provider "aws" {
+  region = "us-west-2"
+  alias  = "us-west-2"
 }
 
 # resource "aws_instance" "example" {
@@ -12,3 +18,45 @@ provider "aws" {
 #     TTL = 48
 #   }
 # }
+
+variable "providers" {
+  default = ["aws.us-east-1", "aws.us-west-2"]
+}
+
+variable "availability_zones" {
+  type = "map"
+
+  default = { 
+    aws.us-east-1 = ["aws.us-east-1a", "aws.us-east-1b"]
+    aws.us-west-2 = ["aws.us-west-2a", "aws.us-west-2b"]
+}
+
+
+resource "aws_instance" "frontend" {
+  count = 4
+
+  provider = "${variable.providers}"
+  availability_zone = "${variable.availability_zones[${aws_instance.frontend.provider}]}"
+
+  instance_type = "t1.micro"
+
+  depends_on = "${aws_instance.backend}"
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_instance" "backend" {
+  count = 4
+
+  provider = "${variable.providers}"
+  availability_zone = "${variable.availability_zones[${aws_instance.frontend.provider}]}"
+
+  instance_type = "t1.micro"
+
+  # lifecycle {
+  #   prevent_destroy = true
+  # }
+}
+
+
